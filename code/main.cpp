92a115d09dbe12743f212fb2298aa97136798252
litchi99 <1040583227@qq.com>
2020-09-19 21:41:00 +0800

    delete psw

--- code/main.cpp
+++ code/main.cpp
@@ -11,7 +11,7 @@ int main() {
     //daemon(1, 0); 
     WebServer server(
         1316, 3, 5000, true,        /* 端口 ET模式 timeoutMs  优雅退出  */
-        "47.115.79.112", 3306, "root", "888888", "cppdb", /* Mysql配置 */
+        "*7.115.79.11*", 3306, "root", "******", "cppdb", /* Mysql配置 */
         12, 6, true, 0, 1024);              /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
     server.Start();
 } 
